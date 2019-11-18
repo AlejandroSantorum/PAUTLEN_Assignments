5fb10d92b1e9929cf428e71c47b2d5cd5120e447
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-11-18 21:38:59 +0100

    fix SIGSEV when creating and deleting hash table

--- assignment3/hash_t_test.c
+++ assignment3/hash_t_test.c
@@ -7,13 +7,17 @@
 int main(){
   hash_tb *ht=NULL;
 
+  printf("Creating Hash table...\n");
   ht = hash_tb_create(16);
   if(!ht){
     printf("Memory Error: Unable to allocate memory for the hash table\n");
     return -1;
   }
+  printf("Created!\n");
 
+  printf("Deleting Hash table...\n");
   hash_tb_delete(ht);
+  printf("Deleted!\n");
 
   return 0;
 }
--- assignment3/hash_table.c
+++ assignment3/hash_table.c
@@ -22,69 +22,105 @@ struct _hash_tb{
 
 
 ht_item * _ht_item_create(){
-  ht_item *item=NULL;
-  item = (ht_item *) calloc(1, sizeof(ht_item));
-  printf("Hdhsajdha");
-  return item;
+    ht_item *item=NULL;
+
+    item = (ht_item *) calloc(1, sizeof(ht_item));
+    if(!item){
+        perror("Unable to allocate memory for ht_item\n");
+        return NULL;
+    }
+    item->key = NULL;
+    return item;
 }
 
 void _ht_item_delete(ht_item *hitem){
-  if(hitem){
-    if(hitem->key) free(hitem->key);
-    free(hitem);
-  }
+    if(hitem){
+        if(hitem->key){
+            free(hitem->key);
+        }
+        free(hitem);
+    }
 }
 
 ht_arr * _ht_arr_create(size_t base_sz){
-  ht_arr *harr=NULL;
+    ht_arr *harr=NULL;
 
-  harr = (ht_arr *) calloc(1, sizeof(ht_arr));
-  harr->base_sz = base_sz;
-  harr->curr_sz = base_sz;
+    harr = (ht_arr *) calloc(1, sizeof(ht_arr));
+    if(!harr){
+        perror("Unable to allocate memory for ht item array");
+        return NULL;
+    }
+    harr->base_sz = base_sz;
+    harr->curr_sz = base_sz;
 
-  for(int i=0; i<base_sz; i++){
-    harr->item_arr[i] = _ht_item_create();
-  }
+    harr->item_arr = (ht_item **) calloc(base_sz, sizeof(ht_item *));
+    if(!(harr->item_arr)){
+        perror("Unable to allocate memory for ht_item array in a ht_arr object");
+        return NULL;
+    }
 
-  return harr;
+    for(int i=0; i<base_sz; i++){
+        if(!(harr->item_arr[i] = _ht_item_create())){
+            printf("Unable to allocate memory for ht_item in array index %d\n", i);
+            return NULL;
+        }
+    }
+    return harr;
 }
 
 void _ht_arr_delete(ht_arr *harr){
-  if(harr){
-    for(int i=0; i<(harr->curr_sz); i++){
-      if(harr->item_arr[i]) _ht_item_delete(harr->item_arr[i]);
+    if(harr){
+        if(harr->item_arr){
+            for(int i=0; i<(harr->curr_sz); i++){
+                if(harr->item_arr[i]) _ht_item_delete(harr->item_arr[i]);
+            }
+            free(harr->item_arr);
+        }
+        free(harr);
     }
-    free(harr);
-  }
 }
 
 
 hash_tb * hash_tb_create(int ht_sz){
-  hash_tb *ht=NULL;
+    hash_tb *ht=NULL;
 
-  if(ht_sz <= 0){
-    perror("Hash table size too low");
-    return NULL;
-  }
+    if(ht_sz <= 0){
+        perror("Hash table size too low");
+        return NULL;
+    }
 
-  ht = (hash_tb *) calloc(1, sizeof(hash_tb));
-  ht->ht_sz = ht_sz;
+    ht = (hash_tb *) calloc(1, sizeof(hash_tb));
+    if(!ht){
+        perror("Unable to allocate memory for hash table");
+        return NULL;
+    }
+    ht->ht_sz = ht_sz;
 
-  ht->ht_arr = (ht_arr **) calloc(ht_sz, sizeof(ht_arr *));
+    ht->ht_arr = (ht_arr **) calloc(ht_sz, sizeof(ht_arr *));
+    if(!(ht->ht_arr)){
+        perror("Unable to allocate memory for ht array");
+        return NULL;
+    }
 
-  for(int i=0; i<ht_sz; i++){
-    ht->ht_arr[i] = _ht_arr_create(ht_sz);
-  }
+    for(int i=0; i<ht_sz; i++){
+        if(!(ht->ht_arr[i] = _ht_arr_create(ht_sz))){
+            printf("Unable to allocate memory for ht array in hash table index %d\n", i);
+            return NULL;
+        }
+    }
 
-  return ht;
+    return ht;
 }
 
 
 void hash_tb_delete(hash_tb *ht){
-  if(ht){
-    for(int i=0; i<ht->ht_sz; i++){
-      if(ht->ht_arr[i]) _ht_arr_delete(ht->ht_arr[i]);
+    if(ht){
+        if(ht->ht_arr){
+            for(int i=0; i<ht->ht_sz; i++){
+                if(ht->ht_arr[i]) _ht_arr_delete(ht->ht_arr[i]);
+            }
+            free(ht->ht_arr);
+        }
+        free(ht);
     }
-    free(ht);
-  }
 }
